c69bced9b9ff435f0c783b65c5a2e751e4d66d4f
Jordan Safford <jordansafford@gmail.com>
2019-09-13 16:50:41 -0700

    create javascript & html files

---
+++ src/lookup-interface.js
@@ -0,0 +1,33 @@
+import { LookupService } from './../js/lookup-service.js';
+
+$(document).ready(function() {
+  $('#doctorLocation').click(function() {
+    let city = $('#userLocation').val();
+    $('#userocation').val("");
+
+
+
+
+    let promise = // code moved to _lookup_service.js
+      let request = new XMLHttpRequest();
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+});
