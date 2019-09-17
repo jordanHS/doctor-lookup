21f16ec733d26f20d941de15ff02e20d6f1d0eb0
Jordan Safford <jordansafford@gmail.com>
2019-09-16 17:49:00 -0700

    modify interface& service files

--- src/lookup-interface.js
+++ src/lookup-interface.js
@@ -3,29 +3,16 @@ import { LookupService } from './../js/lookup-service.js';
 $(document).ready(function() {
   $('#doctorLocation').click(function() {
     let city = $('#userLocation').val();
-    $('#userocation').val("");
+    $('#userLocation').val("");
 
 
 
 
     let promise = // code moved to _lookup_service.js
-      let request = new XMLHttpRequest();
-
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+      $('.showDoctors').text(`The doctors in ${city} is ${body.main.doctors}%`);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
