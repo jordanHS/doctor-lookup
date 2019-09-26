19b2bf3ac6b9bfad0ae0664ea5e20d2289cdf35f
Jordan Safford <jordansafford@gmail.com>
2019-09-25 20:31:25 -0700

    Add env file with API key

--- src/lookup-service.js
+++ src/lookup-service.js
@@ -9,7 +9,7 @@ export  class LookupService {
     getDoctorByPractice(practice) {
         return New Promise(function(resolve, reject) {
             let request = new XMLHttpRequest();
-            const url = `https://api.betterdoctor.com/2016-03-01/practices?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&fields=Internal%20medicine&skip=0&limit=10&apikey8882649c832a2b5e91873e77aa80b644`
+            const url = `https://api.betterdoctor.com/2016-03-01/practices?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&fields=Internal%20medicine&skip=0&limit=10&apikey{exports.apiKey}
             request.onload = function() {
                 if (this.status === 200) {
                     resolve(request.response);
