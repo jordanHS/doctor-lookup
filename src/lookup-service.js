489cae47fcd0901db43a3edec103831061c8f039
Jordan Safford <jordansafford@gmail.com>
2019-09-29 19:46:45 -0700

    update URL

--- src/lookup-service.js
+++ src/lookup-service.js
@@ -5,10 +5,10 @@ export  class LookupService {
     
     
     
-    getDoctorByPractice(practice) {
+    getDoctorByName(name) {
         return new Promise(function(resolve, reject) {
             let request = new XMLHttpRequest();
-            const url = `hhttps://api.betterdoctor.com/2016-03-01/practices?name=Matthew%20Noell&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key={exports.apiKey}`
+            const url = `https://api.betterdoctor.com/2016-03-01/doctors?name={name}location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10={exports.apiKey}`
             request.onload = function() {
                 if (this.status === 200) {
                     resolve(request.response);
